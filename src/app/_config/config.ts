36b1a8a82ea286bf8e48058c520f7bb0c4f818d4
mjunaid-shaikh <shaikhjunaid.4455@gmail.com>
2026-04-03 20:01:45 +0530

    added jsonplaceholder apis

--- src/app/_config/config.ts
+++ src/app/_config/config.ts
@@ -3,4 +3,9 @@ export const CONFIG = {
     getProduct: 'get-product',
     deleteProduct: 'delete-product',
     updateProduct: 'update-product',
+    jsonPlaceHolder: 'https://jsonplaceholder.typicode.com/',
+    posts: 'https://jsonplaceholder.typicode.com/posts/',
+    comments: 'https://jsonplaceholder.typicode.com/comments/',
+    users: 'https://jsonplaceholder.typicode.com/users/',
+    todos: 'https://jsonplaceholder.typicode.com/todos/',
 }
